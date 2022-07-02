1241a990aae1f90890ea456c0a0da9c8b303a05f
Mohammad Hossein Amri <mhamri@gmail.com>
2022-07-03 02:05:33 +0800

    add home & privacy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((x) => x.HomeModule),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'privacy',
+    loadChildren: () =>
+      import('./privacy/privacy.module').then((m) => m.PrivacyModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      routes /* , {
+      anchorScrolling: 'enabled',
+      relativeLinkResolution: 'legacy',
+    } */
+    ),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
